946733bf027b12b4971624eeda678001e9380236
Qilin <qilin.cheng@foxmail.com>
2022-04-21 18:10:48 +0800

    Init Project

---
+++ Source/InsightUE/InsightUEGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InsightUEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INSIGHTUE_API AInsightUEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
